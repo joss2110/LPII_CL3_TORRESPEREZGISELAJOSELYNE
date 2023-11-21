c6887a398ada05f06207e65e844a1f102220c34b
joss2026 <joss2026@hotmail.com>
2023-11-21 17:50:53 -0500

    inicio

---
+++ src/main/java/com/bd/tienda/LpiiCl3TorresperezgiselajoselyneApplication.java
@@ -0,0 +1,13 @@
+package com.bd.tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpiiCl3TorresperezgiselajoselyneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpiiCl3TorresperezgiselajoselyneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bd/tienda/LpiiCl3TorresperezgiselajoselyneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bd.tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LpiiCl3TorresperezgiselajoselyneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
